49bb37574869b9de7357ad2c32e3271370e21d86
GonzalezPerezLuisAngel <luisangelgonzaxl@gmail.com>
2026-02-28 17:31:36 -0600

    Ej3.c

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,24 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 3
+
+int main()
+{
+    int a[3]; 
+    int b[3];
+    int c[3];
+
+    printf("Direcciones de memoria de a\n");    
+    for(int i=0; i<3; i++){    
+        printf("&a[%d] = %p\n", i, &a[i]);
+    }
+
+    printf("Direcciones de memoria de b \n");
+    for(int i=0; i<3; i++){
+        printf("&b[%d] = %p\n", i, &b[i]);
+    }
+
+    printf("Direcciones de memoria de c \n");    
+    for(int i=0; i<3; i++){
+        printf("&c[%d] = %p\n", i, &c[i]);
+    }
     return 0;
 }
