b9ca753270bcd1db811fe56a8dd0cea3b9a9edb0
GonzalezPerezLuisAngel <luisangelgonzaxl@gmail.com>
2026-02-28 17:59:07 -0600

    Ej6.c

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,17 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 6
+
+int main()
+{
+    int m[2][4];
+    int i,j;
+    
+    printf("Las direcciones de memoria son: \n");
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<4; j++);
+        {
+        printf("&m[%d][%d] = %p\n", i,j,&m[i][j]);
+        }
+    }
     return 0;
 }
