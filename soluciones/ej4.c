92955a32996dbd494b1e446e094e0e64d38b5360
GonzalezPerezLuisAngel <luisangelgonzaxl@gmail.com>
2026-02-28 17:51:36 -0600

    Ej4.c

--- soluciones/ej4.c
+++ soluciones/ej4.c
@@ -1,6 +1,12 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 4
+    int a[10];
+
+    printf("&a[0] = %p\n", &a[0]);
+    printf("&a[3] = %p\n", &a[3]);
+    printf("&a[5] = %p\n", &a[5]);
+
+    printf("Tamaño del tipo = %d\n", sizeof(int));
     return 0;
 }
