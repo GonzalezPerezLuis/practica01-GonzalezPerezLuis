09acc0bdea354c4f690fc9662454e418a348ad25
GonzalezPerezLuisAngel <luisangelgonzaxl@gmail.com>
2026-02-28 17:43:18 -0600

    Ej10.c

--- soluciones/ej10.c
+++ soluciones/ej10.c
@@ -1,6 +1,23 @@
+//Corregido para que no falle
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 10
-    return 0;
+include <stdio.h>
+int gigante[1000000];
+
+int main() {
+
+	return 0;
 }
+
+/* Si falla:
+1. explica causa
+Si el arreglo se declara dentro de una función, se almacena dentro del stack,
+este tiene un tamaño de memoria limitado y al querer almacenar un arreglo tan grande
+puede que ocurra un desbordamiento
+
+2. indica dónde se almacena
+Si se declara en una funcion, se almacena en el stack
+
+3. propone solución sin memoria dinámica
+Hacer que el arreglo sea global para que se almacene en la memoria estatica (DATA/BSS)
+
+*/
